b057f6f4862e68c1b5efaa5bd1d49e4307197aa1
cbekoder <cbekoder@gmail.com>
2024-12-11 13:43:28 +0500

    update: fixing filter and adding requirements.txt

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, filters
 from .models import Reja
 from .serializers import RejaSerializer
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class RejaViewSet(viewsets.ModelViewSet):
     queryset = Reja.objects.all()
@@ -9,9 +11,26 @@ class RejaViewSet(viewsets.ModelViewSet):
     ordering_fields = ['sana', 'sarlavha']
     ordering = ['sana']
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                'bajarildi',
+                openapi.IN_QUERY,
+                description="Filter by completion status (true or false)",
+                type=openapi.TYPE_BOOLEAN
+            )
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     def get_queryset(self):
         queryset = super().get_queryset()
         bajarildi = self.request.query_params.get('bajarildi')
         if bajarildi is not None:
+            if bajarildi == 'true':
+                bajarildi = True
+            elif bajarildi == 'false':
+                bajarildi = False
             queryset = queryset.filter(bajarildi=bajarildi)
         return queryset

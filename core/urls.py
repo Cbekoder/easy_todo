d9e4857ffdc1540b6b45ffed30cfaeb2ded6c743
cbekoder <cbekoder@gmail.com>
2024-12-11 14:13:21 +0500

    update url

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reja/', include('main.urls')),
+    path('rejalar/', include('main.urls')),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
